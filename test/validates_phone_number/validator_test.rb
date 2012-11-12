d5bc6fe74fe953a6cd79121313f8f5cbbd11d988
Travis Jeffery <travisjeffery@gmail.com>
2012-11-12 15:09:02 -0500

    add test for both seven and ten digit numbers

--- test/validates_phone_number/validator_test.rb
+++ test/validates_phone_number/validator_test.rb
@@ -71,6 +71,14 @@ class TestValidator < MiniTest::Unit::TestCase
     assert @person.valid?
   end
 
+  def test_validates_ten_or_seven_digit_number
+    Person.validates :phone_number, :phone_number => {:ten_digits => true, :seven_digits => true}
+    @person.phone_number = "555-5555"
+    assert @person.valid?
+    @person.phone_number = "555.555.5555"
+    assert @person.valid?
+  end
+
   def test_validates_seven_digit_number
     Person.validates :phone_number, :phone_number => {:seven_digits => true}
     @person.phone_number = "555-5555"
